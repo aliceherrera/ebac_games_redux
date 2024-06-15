d4e2dcab7710de7bc7fc6767ed2d9d6c16453252
Alice Herrera <a.aliceherrera@gmail.com>
2024-06-15 17:18:48 -0300

    finalizando

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,16 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore } from '@reduxjs/toolkit'
 
 import carinhoReducer from './reducers/carrinho'
 
+import api from '../services/api'
+
 export const store = configureStore({
   reducer: {
-    carrinho: carinhoReducer
-  }
+    carrinho: carinhoReducer,
+    [api.reducerPath]: api.reducer
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware)
 })
 
 export type RootReducer = ReturnType<typeof store.getState>
